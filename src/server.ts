6736e7f2a04ffe2cdf10626d3455d5b9ebaaf45a
Pablo Ribeiro <pablo__br_13@hotmail.com>
2023-04-21 19:03:58 -0300

    Criando rotas e controllers

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,8 @@ import mustache from 'mustache-express';
 import dotenv from 'dotenv';
 import path from 'path';
 
+import mainRoutes from './routes';
+
 dotenv.config();
 
 const server = express();
@@ -12,5 +14,11 @@ server.set('views', path.join(__dirname, 'views'));
 server.engine('mustache', mustache());
 
 server.use(express.static(path.join(__dirname, '../public')));
+
+server.use(mainRoutes);
+
+server.use((req, res) => {
+    res.send('Página não encontrada!');
+});
 
 server.listen(process.env.PORT);
